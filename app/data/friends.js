b83b351f44144b6512065b2699ce542ff5e96bdf
Colin Green <colin.green@knights.ucf.edu>
2019-11-21 15:22:17 -0500

    Got the friend matching functional, as well as the modal. Should be good to go.

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,23 @@ var friendArray = [
         name:"Ahmed",
         photo:"https://static.techspot.com/images2/news/bigimage/2019/01/2019-01-14-image-6.jpg",
         scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-    }
+    },
+    {
+        name: "Colin",
+        photo: "https://scontent.fatl1-2.fna.fbcdn.net/v/t1.0-9/35210614_10214249623207555_2315886687156174848_n.jpg?_nc_cat=108&_nc_ohc=2OlY5wC5mzAAQn2cLsc9O7RvUBpo3vPa7eroZ8leXy9p9IunPmWI-T_iQ&_nc_ht=scontent.fatl1-2.fna&oh=e485381e2b966b4f1ce34827bb3ad0a9&oe=5E7E91F9",
+        scores: [
+        4,
+        4,
+        4,
+        2,
+        5,
+        1,
+        3,
+        3,
+        4,
+        4
+        ]
+        }
 ];
   
   // Note how we export the array. This makes it accessible to other files using require.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,18 +10,56 @@ app.get("/api/friends", function(req, res) {
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // (ex. User fills out the survey... this data is then sent to the server...
   // Then the server saves the data to the friendData array)
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // It will do this by sending out the value "true" have a table
+
     // req.body is available since we're using the body parsing middleware
-      friendData.push(req.body);
-      // res.json(true);
-      res.json({reqBody: req.body});
+    var newUser = req.body;
+
+    // Default best match is the first friend in the array
+    var bestMatchIndex = 0;
+
+    // 40 is the maximum difference anyone can have
+    var maximumDifference = 40;
+
+    // For each friend in the friendData array...
+    for (let i = 0; i < friendData.length; i++) {
+      
+      // set the total difference to 0
+      var totalDifference = 0;
+      
+      // go through each of that friend's scores
+      for (let j = 0; j < friendData[i].scores.length; j++) {
+        
+        // and add the individual difference to the total difference
+        var difference = Math.abs(Number(newUser.scores[j]) - Number(friendData[i].scores[j]));
+        totalDifference += difference;
+        
+      }
+
+      // If their resulting difference is less than the previous maximum...
+      if (totalDifference < maximumDifference) {
+
+        // make it the new best match
+        bestMatchIndex = i;
+
+        // and also make it the new maximum to beat
+        maximumDifference = totalDifference;
+      }
+      
+    }
+
+      // Finally, push the new user to the friendData array (after converting the scores to numbers)
+      newUser.scores = newUser.scores.map(Number);
+      friendData.push(newUser);
+      // Send back a json of the best match
+      res.json(friendData[bestMatchIndex]);
   });
 
+  // Clears the friendData array
   app.post("/api/clear", function(req, res) {
     // Empty out the arrays of data
     friendData.length = 0;
